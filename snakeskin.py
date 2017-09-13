2a30f3c142509b1e525829c360b1d5dea1cac831
admbarre <barrera.adrian.m@gmail.com>
2017-09-12 21:45:00 -0700

    Major refactoring.

--- snakeskin.py
+++ snakeskin.py
@@ -1,27 +1,43 @@
-#!/Users/Adrian/.virtualenvs/slides/bin/python
 from PIL import Image
 import sys, os, datetime, zipfile
 from pathlib import Path
 
+def yesno(prompt):
+    while True:
+        response = input('{} (y/n): '.format(prompt)).lower()
+        if response == 'y':
+            return True
+        elif response == 'n':
+            return False
+        else:
+            print('Invalid input. Try again')
+'''
 if len(sys.argv) < 2:
     sys.exit("Program usage: snakeskin.py [directory]")
 target = Path(sys.argv[1])    # The directory with our images we want scaled
 os.chdir(target)    # Move into our image directory
+'''
 
 today = datetime.date.today().strftime('%m-%d-%y')  # Today's date in our preferred format
+print('Date: {}'.format(today))
+
 ratio = (1920,1080) # Our desired image size
 formats = ('.jpg', '.png')  # Our accepted image formats
 
-images = [Image.open(img) for img in os.listdir() if img.endswith(formats)]   # Opens all our images
+images = [Image.open(img) for img in os.listdir() if img.endswith(formats)]   # Opens all our images as long as the format is supported
 
 # NOTE: Image object loses its original format and filename properties
 filenames = [img.filename for img in images]
-resized_images = [img.resize(ratio) for img in images]
-imgs_with_names= zip(resized_images,filenames)
+print('Editing:')
+for name in filenames:
+    print('+ {}'.format(name))
+if yesno('Resize files?'):
+    resized_images = [img.resize(ratio) for img in images]
+    imgs_with_names= zip(resized_images,filenames)
 
-for img, name in imgs_with_names:
-    img.save(name)
-os.rename(Path.cwd(), Path.cwd() / '..' / today)    # Renames our folder with the current date
+    for img, name in imgs_with_names:
+        img.save(name)
 
-def scale(directory, ratio):
-    os.chdir(directory)
+    if yesno("Rename folder with today's date?"):
+        os.rename(Path.cwd(), Path.cwd() / '..' / today)    # Renames our folder with the current date
+print('~Snakeskin has completed~')
